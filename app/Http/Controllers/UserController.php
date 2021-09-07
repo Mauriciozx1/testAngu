717a5b461b601a4fcf426931d99440c96ace767d
mauricio <mauriciozx1@gmail.com>
2021-09-06 21:57:04 -0300

    backend ok

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\UserRegisterRequest;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index(){
+        return User::all();
+    }
+    public function user(Request $request) {
+        return $request->user();
+    }
+    public function register(UserRegisterRequest $request)
+{
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        return response()->json([
+            'message' => 'success'
+        ]);
+}
+}
